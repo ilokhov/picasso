a1d335761615abad15b00b239cf3c89b150e95a8
ilokhov <ilokhov.dev@gmail.com>
2017-03-15 15:17:45 +0100

    slider attempt

---
+++ js/libs/Leaflet.TimeDimension-master/examples/js/example11.js
@@ -0,0 +1,65 @@
+Date.prototype.format = function (mask, utc) {
+    return dateFormat(this, mask, utc);
+};
+
+var currentMonth = new Date();
+currentMonth.setUTCDate(1, 0, 0, 0, 0);
+
+var map = L.map('map', {
+    zoom: 2,
+    fullscreenControl: true,
+    timeDimension: true,
+    timeDimensionOptions:{
+        timeInterval: "1981-09/" + currentMonth.format("yyyy-mm"),
+        period: "P1M",
+        currentTime: Date.parse("1981-09-01T00:00:00Z")
+    },    
+    center: [20.0, 0.0],
+});
+
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'    
+}).addTo(map);
+
+var proxy = 'server/proxy.php';
+var testWMS = "http://www.ncdc.noaa.gov/thredds/wms/OISST-V2-AVHRR_agg_combined"
+var testLayer = L.tileLayer.wms(testWMS, {
+    layers: 'sst',
+    format: 'image/png',
+    transparent: true,
+    style: 'boxfill/sst_36',
+    colorscalerange: '-3,35',
+    abovemaxcolor: "extend",
+    belowmincolor: "extend",    
+    attribution: '<a href="http://www.ncdc.noaa.gov">NOAAs National Climatic Data Center</a>'
+});
+var testTimeLayer = L.timeDimension.layer.wms(testLayer, {
+    proxy: proxy,
+    updateTimeDimension: false,
+});
+testTimeLayer.addTo(map);
+
+var testLegend = L.control({
+    position: 'topright'
+});
+testLegend.onAdd = function(map) {
+    var src = testWMS + "?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetLegendGraphic&LAYER=sst&PALETTE=sst_36&COLORSCALERANGE=-3,35";
+    var div = L.DomUtil.create('div', 'info legend');
+    div.innerHTML +=
+        '<img src="' + src + '" alt="legend">';
+    return div;
+};
+testLegend.addTo(map);
+
+L.Control.TimeDimensionCustom = L.Control.TimeDimension.extend({
+    _getDisplayDateFormat: function(date){
+        return date.format("dS mmmm yyyy");
+    }    
+});
+var timeDimensionControl = new L.Control.TimeDimensionCustom({
+    playerOptions: {
+        buffer: 1,
+        minBufferReady: -1
+    }
+});
+map.addControl(this.timeDimensionControl);
--- js/script.js
+++ js/script.js
@@ -1,20 +1,56 @@
 window.onload = function() {
   var circles = new L.LayerGroup();
 
-
-  var circle = L.circle([51.508, -0.11], {
-      color: 'red',
-      fillColor: '#f03',
-      fillOpacity: 0.5,
-      radius: 50000
+  var circleGroupOne = new L.LayerGroup({
+    time: "1918",
   }).addTo(circles);
 
-  var circleTwo = L.circle([51.508, -10.11], {
+  var circleGroupTwo = new L.LayerGroup({
+    time: "1920",
+  }).addTo(circles);
+
+
+
+  var circleOne = L.circle([40.415916, -3.703578], {
+      color: 'red',
+      fillColor: 'red',
+      fillOpacity: 0.5,
+      radius: 50000,
+      // time: "1922",
+  }).addTo(circleGroupOne);
+
+  var circleTwo = L.circle([40.715721, -74.026105], {
       color: 'blue',
       fillColor: 'blue',
       fillOpacity: 0.5,
-      radius: 50000
-  }).addTo(circles);
+      radius: 50000,
+      // time: "1918",
+  }).addTo(circleGroupOne);
+
+
+
+
+
+
+  var circleThree = L.circle([40.415916, -3.703578], {
+      color: 'green',
+      fillColor: 'green',
+      fillOpacity: 0.5,
+      radius: 50000,
+      // time: "1920",
+  }).addTo(circleGroupTwo);
+
+  var circleFour = L.circle([40.715721, -74.026105], {
+      color: 'purple',
+      fillColor: 'purple',
+      fillOpacity: 0.5,
+      radius: 50000,
+      // time: "1920",
+  }).addTo(circleGroupTwo);
+
+
+
+
 
 
 
@@ -31,8 +67,21 @@ window.onload = function() {
   var map = L.map('map', {
     center: [51.505, -20.09],
     zoom: 3,
-    layers: [grayscale, circles]
+    layers: [grayscale, circles],
   });
+
+
+
+  var sliderControl = L.control.sliderControl({
+    position: "bottomleft",
+    layer: circles,
+    timeStrLength: 5,
+    alwaysShowDate: true,
+    minValue: 0,
+    maxValue: 10000,
+  });
+  map.addControl(sliderControl);
+  sliderControl.startSlider();
 
 
 
